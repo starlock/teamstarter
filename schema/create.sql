808f0323768ba3cf6df12c569edc8c009451c088
Martin Melin <martin@tictail.com>
2012-06-11 14:57:04 +0200

    add some more fields to users table

--- schema/create.sql
+++ schema/create.sql
@@ -3,5 +3,8 @@ CREATE SEQUENCE users_id_seq;
 CREATE TABLE users (
     id INT NOT NULL UNIQUE DEFAULT NEXTVAL('users_id_seq'),
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    bio VARCHAR(120) NOT NULL DEFAULT 'No bio',
+    created_at TIMESTAMP,
+    modified_at TIMESTAMP
 );
