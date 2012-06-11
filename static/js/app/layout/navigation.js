896447957b761643125d3870e3ea7b2de63fc4e7
Birk Nilson <me@birknilson.se>
2012-06-11 17:38:26 +0200

    Moving frontend handling to JavaScript & adding basic loader

---
+++ static/js/app/layout/navigation.js
@@ -0,0 +1,7 @@
+define([
+    'text!template/layout/navigation.html'
+], function() {
+    var NavigationLayout = Backbone.View.extend({
+    });
+    return NavigationLayout;
+});
--- static/js/app/main.js
+++ static/js/app/main.js
@@ -1,2 +1,31 @@
-define(function() {
+define([
+    'text!template/layout/main.html',
+    './layout/navigation'
+], function(
+    BaseMarkup,
+    NavigationLayout
+) {
+    var ApplicationView = Backbone.View.extend({
+        render: function() {
+            var content = Mustache.render(BaseMarkup, {});
+            this.$el.append(content);
+            return this;
+        }
+    });
+
+    var onDocumentReady = function() {
+        var bodyNode = $('body'),
+            curtainNode = $('#curtain'),
+            container = $('<div/>');
+
+
+        container.append(new ApplicationView().render().$el);
+        curtainNode.fadeOut('slow', function() {
+            bodyNode.append(container.contents());
+        });
+    };
+
+    $(function() {
+        onDocumentReady();
+    });
 });
