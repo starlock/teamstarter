7f4b58e3ffddc82a47b19436efc7750bca7e3fb8
Steeve Lennmark <steevel@handeldsbanken.se>
2012-06-11 14:47:44 +0200

    Unique function names are probably a good idea

--- api/project.py
+++ api/project.py
@@ -3,13 +3,13 @@ import flask
 page = flask.Blueprint("project", __name__)
 
 @page.route("/create", methods=["POST"])
-def login():
+def create():
    return "CREATE PROJECT!"
 
 @page.route("/<int:project_id>", methods=["GET"])
-def login(project_id):
+def fetch(project_id):
    return "Get: %d" % project_id
 
 @page.route("/<int:project_id>", methods=["PUT"])
-def login(project_id):
+def modify(project_id):
    return "Modify project: %d" % project_id
