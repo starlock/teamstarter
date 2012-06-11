ee9662b844af885f483b7c0f6ce85f7a4d7078ec
Martin Melin <martin@tictail.com>
2012-06-11 23:17:53 +0200

    port existing Project methods to model

--- api/project.py
+++ api/project.py
@@ -1,48 +1,20 @@
 from flask import request, Blueprint, session
-from datetime import datetime
 
 import db
 import utils
+from models.project import Project
 
 page = Blueprint("project", __name__)
 
 @page.route("/create", methods=["POST"])
 def create():
-    # request.form["name"]
-    # request.form["description"]
-    # request.form["role"]
+    project = Project.create(request.form["name"], request.form["description"],
+                             session["user_id"], request.form["role"])
 
-    role = 'ADMIN'
-    if 'role' in request.form:
-        role = request.form['role'].upper()
-
-    if 'user_id' not in session:
-        return "Unauthorized", 403 # TODO: use decorator instead
-
-    conn = db.engine.connect()
-    trans = conn.begin()
-    try:
-        data = conn.execute(db.projects.insert(),
-            name = request.form["name"],
-            description = request.form["description"]
-        )
-
-        [project_id] = data.inserted_primary_key
-
-        data = conn.execute(db.user_projects.insert(),
-            user_id = session['user_id'],
-            project_id = project_id,
-            role = role,
-            created_at = datetime.now(),
-            modified_at = datetime.now()
-        )
-
-        trans.commit()
-    except:
-        trans.rollback()
+    if project is None:
         return "Could not create project", 500
 
-    return "Created project: %d" % project_id, 201
+    return db.json_encode(project.to_dict()), 201
 
 @page.route("/<int:project_id>", methods=["GET"])
 def fetch(project_id):
---
+++ models/project.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+import db
+
+from models import BaseModel
+
+class Project(BaseModel):
+
+    properties = db.projects.c.keys()
+
+    @classmethod
+    def get(cls, id):
+        data = db.projects.select().where(db.projects.c.id == id).execute()
+        if data.rowcount == 0:
+            return None
+        row = data.fetchone()
+        return cls.init(**row)
+
+    @classmethod
+    def create(cls, name, description, user_id, role='ADMIN'):
+        role = role.upper()
+        conn = db.engine.connect()
+        trans = conn.begin()
+        try:
+            data = conn.execute(db.projects.insert(),
+                name = name,
+                description = description
+            )
+
+            [id] = data.inserted_primary_key
+
+            created_at = modified_at = datetime.now()
+
+            data = conn.execute(db.user_projects.insert(),
+                user_id = user_id,
+                project_id = id,
+                role = role,
+                created_at = created_at,
+                modified_at = modified_at
+            )
+
+            trans.commit()
+        except:
+            trans.rollback()
+            return None
+
+        return cls.init(id=id, name=name, description=description,
+                        user_id=user_id, modified_at=modified_at,
+                        created_at=created_at)
