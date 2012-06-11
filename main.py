4aa117d0895de0cb891530d5246e1ea9adc724c3
Nisse <comhemnisse@gmail.com>
2012-06-11 17:22:45 +0200

    Added 404 handler

--- main.py
+++ main.py
@@ -16,5 +16,9 @@ def index():
 def signup():
     return render_template('signup.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
