12c94a61101fd1db31eba69384c75f6f2c691622
Steeve Lennmark <steevel@handeldsbanken.se>
2012-06-11 22:27:23 +0200

    Added oauth config

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,15 @@
 DEBUG = True
-DATABASE_URI = 'postgresql://webadmin@/teamstarter'
+SECRET_KEY = "--teamstarter-very-secret-key"
+
+oauth = {
+    "github": {
+        "id": "6e273f26029aee529b63",
+        "secret": "7e54427fa41e0a1a5fb7ea383d06cccd736cf528",
+        "authorize_url": "https://github.com/login/oauth/authorize",
+        "redirect_url": "/api/user/oauth"
+    },
+    "linkedin": {
+        "id": "",
+        "secret": ""
+    }
+}
