d4d628a167e93fc5f824d31e329f4489f9f77cd1
William Tisäter <william@defunct.cc>
2012-06-12 02:56:51 +0200

    User settings form

--- static/js/app/view/user/profile.js
+++ static/js/app/view/user/profile.js
@@ -1,7 +1,19 @@
-define(function() {
+define(['text!template/layout/user/profile.html'],
+function(UserProfileMarkup) {
     var UserProfileView = Backbone.View.extend({
+        events: {
+            'click #user-save-btn': 'onUserProfileClick'
+        },
+
+        onUserProfileClick: function(e) {
+            e.preventDefault();
+            console.log('onUserProfileClick');
+        },
+
         render: function() {
-            var content = this.make('h1', {}, 'Hello user profile view');
+            var params = {};
+            params.user = window.session.attributes;
+            var content = Mustache.render(UserProfileMarkup, params);
             this.$el.append(content);
             return this;
         }
